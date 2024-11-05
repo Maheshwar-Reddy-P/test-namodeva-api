6ef5bdbb187e714a91ae67e4a4907cd5fa889314
Mahesh <pmaheshwarreddy243@gmail.com>
2024-11-05 11:27:36 +0530

    first commit

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple route
+app.get('/login', (req, res) => {
+    res.send('User logged in Successfully!');
+});
+
+app.get('/welcome', (req, res) => {
+    res.send('Welcome to AWS Lambda Function!');
+});
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on ${PORT}...`);
+});
+
+// Export the handler for AWS Lambda
+export const handler = serverless(app);
